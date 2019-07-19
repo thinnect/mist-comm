e7db040fad0277143379f67a4280c1aaeb1027b9
Raido Pahtma <raidopahtma@gmail.com>
2019-07-19 17:56:11 +0300

    Add bcast addr to basics header.

--- include/compat/mist_comm_basics.h
+++ include/compat/mist_comm_basics.h
@@ -5,6 +5,8 @@ typedef uint8_t am_group_t; // This is actually PAN
 typedef uint8_t am_id_t;
 typedef uint16_t am_addr_t;
 
+#define AM_BROADCAST_ADDR ((uint16_t)0xFFFF)
+
 typedef struct ieee_eui64 {
 	uint8_t data[8];
 } ieee_eui64_t;
