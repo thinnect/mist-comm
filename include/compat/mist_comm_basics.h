7d6289c2067b451d537ba6c726208d6d8b4f6bc1
Raido Pahtma <raidopahtma@gmail.com>
2019-09-19 16:11:53 +0300

    Missing include.

--- include/compat/mist_comm_basics.h
+++ include/compat/mist_comm_basics.h
@@ -1,15 +1,17 @@
 #ifndef MIST_COMM_BASICS
 #define MIST_COMM_BASICS
 
+#include <stdint.h>
+
 typedef uint8_t am_group_t; // This is actually PAN
 typedef uint8_t am_id_t;
 typedef uint16_t am_addr_t;
-
-#define AM_BROADCAST_ADDR ((uint16_t)0xFFFF)
 
 typedef struct ieee_eui64 {
 	uint8_t data[8];
 } ieee_eui64_t;
 // ??? this should come from some ieee_eui64.h header
+
+#define AM_BROADCAST_ADDR ((uint16_t)0xFFFF)
 
 #endif//MIST_COMM_BASICS
