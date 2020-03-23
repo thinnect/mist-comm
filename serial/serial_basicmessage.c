ea504cf0f917d9455048a54e252b3de6ae768866
Raido Pahtma <raidopahtma@gmail.com>
2020-03-23 22:20:50 +0200

    Add serial basicmessage implementation.

---
+++ serial/serial_basicmessage.c
@@ -0,0 +1,247 @@
+/**
+* MistComm SerialBasicMessage layer implementation.
+*
+* @author Raido Pahtma
+* @license MIT
+*/
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <inttypes.h>
+
+#include "cmsis_os2.h"
+
+#include "mist_comm.h"
+#include "serial_protocol.h"
+#include "serial_basicmessage.h"
+
+#include "loglevels.h"
+#define __MODUUL__ "sbm"
+#define __LOG_LEVEL__ (LOG_LEVEL_serial_basicmessage & BASE_LOG_LEVEL)
+#include "log.h"
+
+static comms_error_t serial_basicmessage_send (comms_layer_iface_t * iface,
+                                               comms_msg_t * msg,
+                                               comms_send_done_f * send_done, void * user);
+static bool serial_bm_receive(uint8_t dspch, const uint8_t data[], uint8_t length, void* user);
+static void serial_bm_senddone(uint8_t dspch, const uint8_t data[], uint8_t length, bool acked, void* user);
+static void serial_bm_timer_cb(void * argument);
+
+comms_layer_t* serial_basicmessage_init (serial_basicmessage_t * sbm, serial_protocol_t * spr, uint8_t dispatch)
+{
+	sbm->mutex = osMutexNew(NULL);
+	sbm->timer = osTimerNew(&serial_bm_timer_cb, osTimerOnce, sbm, NULL);
+
+	// Initialize send queueing system
+	sbm->send_busy = false;
+	sbm->sending = NULL;
+	sbm->send_queue = NULL;
+	sbm->free_queue = &(sbm->queue_memory[0]);
+	sbm->free_queue->next = NULL;
+	for (uint8_t i=1; i<sizeof(sbm->queue_memory)/sizeof(sbm_queue_element_t); i++)
+	{
+		sbm->queue_memory[i].next = sbm->free_queue;
+		sbm->free_queue = &(sbm->queue_memory[i]);
+	}
+
+	// Set up dispatcher
+	sbm->protocol = spr;
+	serial_protocol_add_dispatcher(sbm->protocol, dispatch,
+		                           &(sbm->dispatcher),
+		                           &serial_bm_receive, &serial_bm_senddone,
+		                           sbm);
+
+	// Set up the mist-comm layer
+	// TODO start-stop handlers
+	comms_am_create((comms_layer_t *)sbm, 0, &serial_basicmessage_send, NULL, NULL);
+
+	// serial_basicmessage_t is a semi-valid comms_layer_t
+	return (comms_layer_t *)sbm;
+}
+
+bool serial_basicmessage_deinit (serial_basicmessage_t* sbm)
+{
+	while (osOK != osMutexAcquire(sbm->mutex, osWaitForever));
+
+	if ((1 == osTimerIsRunning(sbm->timer))
+	  ||(NULL != sbm->sending))
+	{
+		osMutexRelease(sbm->mutex);
+		return false;
+	}
+
+	serial_protocol_remove_dispatcher(sbm->protocol, &(sbm->dispatcher));
+	osTimerDelete(sbm->timer);
+
+	osMutexRelease(sbm->mutex);
+	osMutexDelete(sbm->mutex);
+
+	return true;
+}
+
+static bool serial_bm_receive(uint8_t dispatch, const uint8_t data[], uint8_t length, void * user)
+{
+	serial_basicmessage_t * sbm = (serial_basicmessage_t*)user;
+	comms_layer_t * lyr = (comms_layer_t*)sbm;
+	debugb1("%p data", data, length, sbm);
+
+	while (osOK != osMutexAcquire(sbm->mutex, osWaitForever));
+
+	comms_msg_t msg;
+	comms_init_message(lyr, &msg);
+
+	uint8_t* payload = comms_get_payload(lyr, &msg, length);
+	if (NULL == payload)
+	{
+		osMutexRelease(sbm->mutex);
+		err1("pl %d", length);
+		return false;
+	}
+
+	comms_set_packet_type(lyr, &msg, 0);
+	comms_set_payload_length(lyr, &msg, length);
+	memcpy(payload, data, length);
+
+	// comms_set_timestamp(lyr, &msg, timestamp); // TODO Set to now? Make the lowest layer register frame starts ...?
+
+	comms_am_set_destination(lyr, &msg, 0);
+	comms_am_set_source(lyr, &msg, 0);
+
+	debugb1("rx {%02"PRIX8"}",
+		payload, comms_get_payload_length(lyr, &msg),
+		dispatch);
+
+	comms_deliver(lyr, &msg);
+
+    osMutexRelease(sbm->mutex);
+
+	return true;
+}
+
+static void serial_bm_senddone(uint8_t dispatch, const uint8_t data[], uint8_t length, bool acked, void * user)
+{
+	serial_basicmessage_t * sbm = (serial_basicmessage_t*)user;
+	comms_layer_t * lyr = (comms_layer_t*)sbm;
+
+	debugb1("snt(a:%d) %02X", data, length, (int)acked, (unsigned int)dispatch);
+
+	// Set flags on the message that was just sent
+	//comms_set_timestamp(lyr, sbm->sending->msg, now());
+	if (acked)
+	{
+		_comms_set_ack_received(lyr, sbm->sending->msg);
+	}
+
+	// Signal send done events
+	sbm->sending->send_done(lyr, sbm->sending->msg, COMMS_SUCCESS, sbm->sending->user);
+	sbm->sending->msg = NULL;
+
+	// Mark sent and defer to send other pending messages
+	while (osOK != osMutexAcquire(sbm->mutex, osWaitForever));
+	sbm->send_busy = false;
+	osMutexRelease(sbm->mutex);
+	osTimerStart(sbm->timer, 1UL);
+}
+
+static void serial_bm_timer_cb(void * argument)
+{
+	serial_basicmessage_t * sbm = (serial_basicmessage_t*)argument;
+	comms_layer_t * lyr = (comms_layer_t*)sbm;
+
+	while (osOK != osMutexAcquire(sbm->mutex, osWaitForever));
+	if(sbm->send_busy)
+	{
+		debug1("bsy"); // Must wait for last send to complete
+		osMutexRelease(sbm->mutex);
+		return;
+	}
+
+	if (NULL != sbm->sending)
+	{
+		// Return the queue element to the free queue
+		sbm->sending->next = sbm->free_queue;
+		sbm->free_queue = sbm->sending;
+		sbm->sending = NULL;
+	}
+
+	if (NULL != sbm->send_queue)
+	{
+		sbm->sending = sbm->send_queue;
+		sbm->send_queue = sbm->send_queue->next;
+		sbm->send_busy = true;
+	}
+	osMutexRelease(sbm->mutex);
+
+	if (NULL != sbm->sending)
+	{
+		uint16_t length = comms_get_payload_length(lyr, sbm->sending->msg);
+		void * payload = comms_get_payload(lyr, sbm->sending->msg, length);
+		if (NULL != payload)
+		{
+			if(true == serial_protocol_send(&(sbm->dispatcher), payload, length, false))
+			{
+				debug1("snd %p %p %p", sbm->sending, sbm->send_queue, sbm->free_queue);
+				return;
+			}
+			else
+			{
+				err1("busy?");
+			}
+		}
+		else
+		{
+			err1("payload");
+		}
+
+		// Something bad has happened, return message to user with error
+		sbm->sending->send_done(lyr, sbm->sending->msg, COMMS_EINVAL, sbm->sending->user);
+		sbm->sending->msg = NULL; // Pointer has been returned
+
+		while (osOK != osMutexAcquire(sbm->mutex, osWaitForever));
+		sbm->send_busy = false;
+		osMutexRelease(sbm->mutex);
+		osTimerStart(sbm->timer, 1L); // Try next message
+	}
+	else
+	{
+		debug1("idle");
+	}
+}
+
+static comms_error_t serial_basicmessage_send (comms_layer_iface_t * iface,
+                                               comms_msg_t * msg,
+                                               comms_send_done_f * send_done, void * user)
+{
+	comms_error_t result = COMMS_SUCCESS;
+	serial_basicmessage_t * sbm = (serial_basicmessage_t*)iface;
+	while (osOK != osMutexAcquire(sbm->mutex, osWaitForever));
+
+	debug1("snd %p %p", sbm->send_queue, sbm->free_queue);
+	if (NULL != sbm->free_queue)
+	{
+		sbm_queue_element_t ** qe = &(sbm->send_queue);
+		while (NULL != *qe)
+		{
+			qe = &((*qe)->next);
+		}
+
+		*qe = sbm->free_queue;
+		sbm->free_queue = sbm->free_queue->next;
+
+		(*qe)->msg = msg;
+		(*qe)->send_done = send_done;
+		(*qe)->user = user;
+		(*qe)->next = NULL;
+
+		debug1("q %p %p", sbm->send_queue, sbm->free_queue);
+		osTimerStart(sbm->timer, 1UL);
+	}
+	else // Queue full
+	{
+		result = COMMS_EBUSY;
+	}
+
+	osMutexRelease(sbm->mutex);
+
+	return result;
+}
---
+++ serial/serial_basicmessage.h
@@ -0,0 +1,81 @@
+/**
+* MistComm SerialBasicMessage layer.
+*
+* Many instances can be created, each needs a serial_protocol.
+*
+* Copyright Thinnect Inc. 2020
+* @author Raido Pahtma
+* @license MIT
+*/
+#ifndef SERIAL_BASICMESSAGE_H_
+#define SERIAL_BASICMESSAGE_H_
+
+#include "mist_comm_am.h"
+#include "mist_comm.h"
+#include "serial_protocol.h"
+
+#include "cmsis_os2.h"
+
+// The serial_basicmessage instance structure
+typedef struct serial_basicmessage serial_basicmessage_t;
+
+/**
+ * Initialize the SerialbasicMessage layer, providing it memory and access
+ * to a lower layer SerialProtocol.
+ *
+ * @param sbm - The SerialBasicMessage to initialize, a pointer to a persistent
+ *              memory structure.
+ * @param spr - Pointer to an initialized SerialProtocol layer.
+ * @param dispatch - The SerialProtocol dispatch ID to use.
+ *
+ * @return a MistComm instance that can be used for sending/receiving messages
+ *         or NULL for failure.
+ */
+comms_layer_t* serial_basicmessage_init (serial_basicmessage_t * sbm,
+                                         serial_protocol_t * spr,
+                                         uint8_t dispatch);
+
+/**
+ * Deinitialize the SerialbasicMessage layer. The memory used by the instance
+ * may be freed after this.
+ *
+ * @param sbm - An initialized SerialbasicMessage
+ * @return true if successful, false if busy
+ */
+bool serial_basicmessage_deinit (serial_basicmessage_t * sbm);
+
+
+// Internal details to allow memory allocation----------------------------------
+
+// send queue structure
+typedef struct sbm_queue_element sbm_queue_element_t;
+struct sbm_queue_element
+{
+	comms_msg_t* msg;
+	comms_send_done_f *send_done;
+	void *user;
+	sbm_queue_element_t* next;
+};
+
+#ifndef SERIAL_BASICMESSAGE_QUEUE_LENGTH
+#define SERIAL_BASICMESSAGE_QUEUE_LENGTH 2
+#endif//SERIAL_BASICMESSAGE_QUEUE_LENGTH
+
+struct serial_basicmessage
+{
+	comms_layer_am_t base;
+	serial_dispatcher_t dispatcher;
+	serial_protocol_t* protocol;
+
+	osMutexId_t mutex;
+	osTimerId_t timer;
+
+	sbm_queue_element_t * sending;
+	sbm_queue_element_t * send_queue;
+	sbm_queue_element_t * free_queue;
+	sbm_queue_element_t queue_memory[SERIAL_BASICMESSAGE_QUEUE_LENGTH];
+
+	bool send_busy;
+};
+
+#endif//SERIAL_BASICMESSAGE_H_
