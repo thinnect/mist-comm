47771c802a16356e96f27e1c996a8c2521bd0e4c
Raido Pahtma <raidopahtma@gmail.com>
2020-04-28 08:58:28 +0300

    Use eui64.h header.

--- include/compat/mist_comm_basics.h
+++ include/compat/mist_comm_basics.h
@@ -3,14 +3,11 @@
 
 #include <stdint.h>
 
+#include "eui64.h"
+
 typedef uint8_t am_group_t; // This is actually PAN
 typedef uint8_t am_id_t;
 typedef uint16_t am_addr_t;
-
-typedef struct ieee_eui64 {
-	uint8_t data[8];
-} ieee_eui64_t;
-// ??? this should come from some ieee_eui64.h header
 
 #define AM_BROADCAST_ADDR ((uint16_t)0xFFFF)
 
