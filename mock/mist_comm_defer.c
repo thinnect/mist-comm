c4a8175c3bf4a6fb232ec8a0c6da60587aca29c7
Raido Pahtma <raidopahtma@gmail.com>
2020-07-02 22:48:50 +0300

    Add mock deferred.

---
+++ mock/mist_comm_defer.c
@@ -0,0 +1,23 @@
+/**
+ * MistComm mock-non-functional deferred implementation.
+ *
+ * Copyright Thinnect Inc. 2020
+ * @license MIT
+ */
+
+#include "mist_comm_private.h"
+
+void _comms_deferred_init(comms_layer_t * comms, void ** deferred, comms_deferred_f * cb)
+{
+
+}
+
+void _comms_defer(void * deferred)
+{
+
+}
+
+void _comms_deferred_deinit(void * deferred)
+{
+
+}
