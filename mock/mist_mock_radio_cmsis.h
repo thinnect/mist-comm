1d2609ba0388c4cc9f151b25f6dcb3d5512fa602
Raido <raidopahtma@gmail.com>
2020-10-21 00:22:50 +0300

    Add a mock radio for testing.

---
+++ mock/mist_mock_radio_cmsis.h
@@ -0,0 +1,21 @@
+/**
+ * MistComm mock radio layer.
+ * Exports outgoing messages, giving send done events to users.
+ *
+ * Copyright Thinnect Inc. 2020
+ * @license MIT
+ */
+#ifndef MIST_MOCK_RADIO_CMSIS_H_
+#define MIST_MOCK_RADIO_CMSIS_H_
+
+#include "mist_comm_am.h"
+
+/**
+ * Create a mock radio.
+ * @param address - radio address.
+ * @param send_copy - function to call with outgoing messages.
+ * @return Mock radio pointer.
+ */
+comms_layer_t * mist_mock_cmsis_radio_init (am_addr_t address, comms_send_f * send_copy);
+
+#endif//MIST_MOCK_RADIO_CMSIS_H_
